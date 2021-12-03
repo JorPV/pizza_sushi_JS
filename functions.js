4d095926f11da2eea0e318d4be779e164189afe6
JordiPV <88150345+JorPV@users.noreply.github.com>
2021-12-03 15:10:50 +0100

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,29 @@
+// Making pizza
+const makingPizza = function () {
+    console.log('Prepare all ingredients');
+    console.log('Get the ingredients for the dough in a bol');
+    console.log('Work with your hands the dough mix, until the right consistency');
+    console.log('Roll the dough until you have a thin pizza base');
+    console.log('Spread the pizza ingredients over the dough base');
+    console.log('Bake the pizza for about 15 minutes in the oven');
+    console.log('Buon appetito!');
+};
+
+makingPizza();
+makingPizza();
+
+// Making sushi
+const makeSushi = function () {
+    console.log('Boil the rice and prepare all ingredients');
+    console.log('Cut all veggies, fish, or fillings you are going to use');
+    console.log('Get the nori (seaweed leaves)');
+    console.log('Once the rice cooked and cooled down, spread a thin layer of rice over a nori leave');
+    console.log('On top of the rice put a few of the ingredients you want to have');
+    console.log('Roll the nori leave with the ingridients and the rice inside it');
+    console.log('Cut the roll in pieces of aproximmately 3 cm'); 
+    console.log('どうぞ召し上がって下さい');
+};
+
+makeSushi();
+makeSushi();
+makeSushi();
